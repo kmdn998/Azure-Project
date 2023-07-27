af69ed45aebe7272465bd5bddb301c28afda9af8
kimnayoung <kny200208@naver.com>
2023-07-28 01:32:09 +0900

    카카오로그인 제대로 구현

--- Mueller-backend/login2.py
+++ Mueller-backend/login2.py
@@ -1,23 +1,26 @@
 from flask import Flask, request, jsonify
 import jwt
 import requests
+import logging
+
 
 app = Flask(__name__)
+
+# Configure logging
+logging.basicConfig(filename="/Users/ny/Desktop/pythonn/app.log", level=logging.DEBUG)
 
 
 @app.route("/login", methods=["POST"])
 def login():
     kakao_token = request.json.get("access_token")
+    app.logger.info("Received token: %s", kakao_token)
 
-    # 카카오 토큰을 카카오에 검증 요청
     headers = {
         "Authorization": f"Bearer {kakao_token}",
     }
     response = requests.get("https://kapi.kakao.com/v2/user/me", headers=headers)
     kakao_user = response.json()
-    # 여기에서 데이터베이스에 사용자를 저장하거나 업데이트하면 됩니다.
 
-    # 사용자 정보로 JWT 생성
     token = jwt.encode(
         {"email": kakao_user["email"], "gender": kakao_user["gender"]},
         "Kknnyy0819@@!",
@@ -28,4 +31,4 @@ def login():
 
 
 if __name__ == "__main__":
-    login2.run(port=5502)
+    app.run(port=5502)
--- Mueller_1_0_0/login2.js
+++ Mueller_1_0_0/login2.js
@@ -1,30 +1,31 @@
-// Kakao JavaScript SDK를 로딩해야 합니다.
-// 로딩 후, Kakao SDK의 초기화와 로그인 버튼에 이벤트 핸들러를 설정합니다.
-Kakao.init("8cd9a7b988003d6821148374f32e3cda");
+document.addEventListener("DOMContentLoaded", function () {
+  // Kakao JavaScript SDK의 초기화와 로그인 버튼에 이벤트 핸들러를 설정합니다.
+  Kakao.init("8cd9a7b988003d6821148374f32e3cda");
 
-document
-  .getElementById("kakao-login-btn")
-  .addEventListener("click", function () {
-    Kakao.Auth.login({
-      success: function (authObj) {
-        // 로그인 성공 시, 백엔드에 토큰을 전송합니다.
-        fetch("http://localhost:3000/login", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            access_token: authObj.access_token,
-          }),
-        })
-          .then((response) => response.json())
-          .then((data) => {
-            // 백엔드에서 받은 JWT를 로컬 저장소에 저장합니다.
-            localStorage.setItem("jwt", data.token);
-          });
-      },
-      fail: function (err) {
-        console.log(err);
-      },
+  document
+    .getElementById("kakao-login-btn")
+    .addEventListener("click", function () {
+      Kakao.Auth.login({
+        success: function (authObj) {
+          // 로그인 성공 시, 백엔드에 토큰을 전송합니다.
+          fetch("http://localhost:5502/login", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              access_token: authObj.access_token,
+            }),
+          })
+            .then((response) => response.json())
+            .then((data) => {
+              // 백엔드에서 받은 JWT를 로컬 저장소에 저장합니다.
+              localStorage.setItem("jwt", data.token);
+            });
+        },
+        fail: function (err) {
+          console.log(err);
+        },
+      });
     });
-  });
+});
